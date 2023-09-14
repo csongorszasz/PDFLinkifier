d73f26be1aaf0a7a84296573ad6d754a67238266
csongorszasz <szasz.csongor1@gmail.com>
2023-09-14 16:07:34 +0300

    Preprocess image; add spinning wheel; show index of file out all files on status bar
    Preprocess each image before running the OCR (OCR not implemented yet).
    Add spinning wheel.
    Show on status bar the number of the file that is currently being processed out of the number of files.

--- linkifier.py
+++ linkifier.py
@@ -1,8 +1,22 @@
-import time
 import cv2
 import pytesseract
 import fitz
 import numpy as np
+
+
+def preprocess_image(cv_image):
+    img = cv_image.copy()
+
+    # create a mask for top section of the page, because we are only interested in the title
+    mask = np.zeros(img.shape[:2], dtype="uint8")
+    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 100), 255, -1)
+
+    # blur, threshold, mask
+    img = cv2.bitwise_and(img, img, mask=mask)
+    img = cv2.GaussianBlur(img, (1, 1), 0)
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    return img
 
 
 def linkify(filepath: str):
@@ -14,25 +28,23 @@ def linkify(filepath: str):
     - for each page
         - convert the page into an image
         - preprocess the image to make the OCR's job easier
+            - mask
         - look for a title at the top section of the image
         - if found
             - add physical text on top of the title, so that it can be searched
             - save a (title, page_number) for later for the contents page
     """
 
-    doc = fitz.open(filepath)
+    doc = fitz.open(filepath)  # open the pdf
     page_num = 1
     for page in doc:
-        # render page to an image and load it in OpenCV
-        pixmap = page.get_pixmap()
-        img = cv2.imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+        pixmap = page.get_pixmap()  # render page to an image
+        img = cv2.imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), cv2.IMREAD_GRAYSCALE)  # load it in OpenCV
+        img = preprocess_image(img)  # prepare image for OCR
 
         cv2.imshow("Image", img)
-        cv2.waitKey(4000)
+        cv2.waitKey(0)
 
         page_num += 1
+
     cv2.destroyAllWindows()
-
-
-    # time.sleep(3)
-
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QFileDialog, QStatusBar
 from PyQt5 import uic
+from PyQt5.Qt import QColor
+from pyqtspinner import WaitingSpinner
 import sys
 
 import linkifier
@@ -33,24 +35,42 @@ class MainWindow(QMainWindow):
             self.print_to_statusbar("Nincs kiválasztott fájl", color="red")
         else:
             self.centralWidget().setEnabled(False)
+            spinner = WaitingSpinner(
+                parent=self.ok_button,
+                roundness=100.0,
+                fade=30.0,
+                radius=3,
+                lines=125,
+                line_length=5,
+                line_width=1,
+                speed=1.5707963267948966,
+                color=QColor(0, 0, 0)
+            )
+            spinner.start()
+            self.ok_button.setText("")
 
-            self.print_to_statusbar("Fájl(ok) feldolgozása folyamatban", color="black", hold_time=0)
-
+            i = 1
             for file in self.chosen_files:
-                self.print_to_statusbar(f"{get_filename_from_path(file)} ...", hold_time=0)
+                self.print_to_statusbar(f"{i}/{len(self.chosen_files)} {get_filename_from_path(file)}", hold_time=0)
                 linkifier.linkify(file)
+                i += 1
 
-            self.browsed_filename_label.setText("")
-            self.print_to_statusbar("Siker", color="green")
+            self.print_to_statusbar("Siker!", color="green")
 
-            self.chosen_files = []
-            self.centralWidget().setEnabled(True)
+            spinner.stop()
+            self.reset()
 
     def print_to_statusbar(self, msg, color="black", hold_time=3000):
         """hold_time is in miliseconds"""
         self.statusBar().setStyleSheet(f"color: {color}")
         self.statusBar().showMessage(msg, hold_time)
         self.statusBar().repaint()
+
+    def reset(self):
+        self.chosen_files = []
+        self.ok_button.setText("OK")
+        self.browsed_filename_label.setText("0 fájl")
+        self.centralWidget().setEnabled(True)
 
 if __name__ == "__main__":
     app = QApplication([])
