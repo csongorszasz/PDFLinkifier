b9f1b88e12223cbf2e6a363dcc891f755eada198
csongorszasz <szasz.csongor1@gmail.com>
2023-09-15 21:45:01 +0300

    [Feature] Integrated Tesseract-OCR to convert the images to text, but it is largely inaccurate

--- linkifier.py
+++ linkifier.py
@@ -9,7 +9,7 @@ def preprocess_image(cv_image):
 
     # create a mask for top section of the page, because we are only interested in the title
     mask = np.zeros(img.shape[:2], dtype="uint8")
-    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 100), 255, -1)
+    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 75), 255, -1)
 
     # blur, threshold, mask
     img = cv2.bitwise_and(img, img, mask=mask)
@@ -17,6 +17,10 @@ def preprocess_image(cv_image):
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
     return img
+
+
+def get_title(cv_image):
+    return pytesseract.image_to_string(cv_image, lang='hun')
 
 
 def linkify(filepath: str):
@@ -29,6 +33,8 @@ def linkify(filepath: str):
         - convert the page into an image
         - preprocess the image to make the OCR's job easier
             - mask
+            - blur
+            - threshold
         - look for a title at the top section of the image
         - if found
             - add physical text on top of the title, so that it can be searched
@@ -42,9 +48,11 @@ def linkify(filepath: str):
         img = cv2.imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), cv2.IMREAD_GRAYSCALE)  # load it in OpenCV
         img = preprocess_image(img)  # prepare image for OCR
 
-        # cv2.imshow("Image", img)
-        # cv2.waitKey(0)
+        print(get_title(img))
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
 
         page_num += 1
 
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
