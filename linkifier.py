66b71e8080743dd69d7dd1fd62247c3f4d8be53a
csongorszasz <szasz.csongor1@gmail.com>
2023-09-16 22:29:57 +0300

    [Optimization] Improved the OCR
    Put a 10px black outer border around the image in the preprocessing stage, which significantly improved the results, but its still far from great.

--- linkifier.py
+++ linkifier.py
@@ -2,19 +2,49 @@ import cv2
 import pytesseract
 import fitz
 import numpy as np
+import unicodedata
+
+
+def strip_accents(s):
+    nfkd_form = unicodedata.normalize('NFKD', s)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 
 def preprocess_image(cv_image):
     img = cv_image.copy()
 
-    # create a mask for top section of the page, because we are only interested in the title
-    mask = np.zeros(img.shape[:2], dtype="uint8")
-    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 75), 255, -1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
 
-    # blur, threshold, mask
-    img = cv2.bitwise_and(img, img, mask=mask)
+    # create a mask for top section of the page, because we are only interested in the title
+    mask = np.zeros(img.shape[:2], dtype=img.dtype)
+    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 100), 255, -1)
+
+    img = cv2.bitwise_and(img, img, mask=mask)  # apply mask
+
+
+    # blur, threshold, border
     img = cv2.GaussianBlur(img, (1, 1), 0)
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    img = cv2.copyMakeBorder(img, 10, 0, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])  # outer top border for the image
+
+    return img
+
+
+def put_bboxes(cv_image):
+    img = cv_image.copy()
+
+    img_height = img.shape[0]
+    boxes = pytesseract.image_to_boxes(img, lang='hun')
+    for box in boxes.splitlines():
+        if box != "":
+            box = box.split(" ")
+            c, x_botleft, y_botleft, x_topright, y_topright = box[0], int(box[1]), int(box[2]), int(box[3]), int(box[4])
+            cv2.rectangle(img, (x_botleft, img_height-y_botleft), (x_topright, img_height-y_topright), color=(255, 255, 255), thickness=1)
+            cv2.putText(img, strip_accents(c), ((x_botleft + x_topright) // 2 - 3, img_height - y_topright + 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+
+    # boxes = pytesseract.image_to_data(img, lang='hun')
+    # for box in boxes.splitlines():
+    #     print(box)
 
     return img
 
@@ -45,10 +75,11 @@ def linkify(filepath: str):
     page_num = 1
     for page in doc:
         pixmap = page.get_pixmap()  # render page to an image
-        img = cv2.imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), cv2.IMREAD_GRAYSCALE)  # load it in OpenCV
+        img = cv2.imdecode(np.frombuffer(pixmap.tobytes(), dtype=np.uint8), cv2.IMREAD_COLOR)  # load it in OpenCV
         img = preprocess_image(img)  # prepare image for OCR
 
         print(get_title(img))
+        img = put_bboxes(img)
 
         cv2.imshow("Image", img)
         cv2.waitKey(0)
