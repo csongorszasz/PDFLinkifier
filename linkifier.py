b35815f6583ce3e2470dfc068d4819619d8ed7f9
csongorszasz <szasz.csongor1@gmail.com>
2023-09-17 16:26:45 +0300

    [Optimization] Improved the OCR - close to perfect accuracy
    Crop the image to the title.
    Apply OCR with Page Segmentation Mode 7 - assumes that the image is a single line of text.

--- linkifier.py
+++ linkifier.py
@@ -1,3 +1,5 @@
+from typing import Sequence
+
 import cv2
 import pytesseract
 import fitz
@@ -10,22 +12,45 @@ def strip_accents(s):
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 
+def get_title_coordinates(cv_image) -> Sequence[int]:
+    """Returns the topleft and coordinate, witdth and height (x,y,w,h) of a bounding box that covers the title."""
+    img = cv_image.copy()
+
+    # erode, dilate
+    img = cv2.erode(img, np.ones((3, 3), np.uint8))  # get rid of any noise
+    img = cv2.dilate(img, cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(50, 1)), iterations=1)  # dilate horizontally
+
+    cv2.imshow("dilated", img)
+
+    # find largest contour
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    largest_cnt = max(contours, key=cv2.contourArea)
+
+    return cv2.boundingRect(largest_cnt)
+
+
 def preprocess_image(cv_image):
+    """Returns a binary image with a single line of text."""
+
     img = cv_image.copy()
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # grayscale
+    img = img[:86, 50:-50]  # crop image to top section
 
-    # create a mask for top section of the page, because we are only interested in the title
-    mask = np.zeros(img.shape[:2], dtype=img.dtype)
-    cv2.rectangle(mask, (50, 0), (img.shape[1] - 50, 100), 255, -1)
-
-    img = cv2.bitwise_and(img, img, mask=mask)  # apply mask
-
-
-    # blur, threshold, border
+    # blur, threshold
     img = cv2.GaussianBlur(img, (1, 1), 0)
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    img = cv2.copyMakeBorder(img, 10, 0, 0, 0, cv2.BORDER_CONSTANT, value=[0,0,0])  # outer top border for the image
+
+    # crop image to title
+    x, y, w, h = get_title_coordinates(img)
+    img = img[(y-5 if y-5 >= 0 else 0):y+h+5, x:x+w]
+
+    img = cv2.erode(img, np.ones((1, 1), np.uint8))  # get rid of any noise
+
+
+    # border
+    border_size = 10
+    img = cv2.copyMakeBorder(img, border_size, border_size, border_size, border_size, cv2.BORDER_CONSTANT, value=[0,0,0])
 
     return img
 
@@ -34,15 +59,15 @@ def put_bboxes(cv_image):
     img = cv_image.copy()
 
     img_height = img.shape[0]
-    boxes = pytesseract.image_to_boxes(img, lang='hun')
+    boxes = pytesseract.image_to_boxes(img, lang='hun', config='--psm 7')
     for box in boxes.splitlines():
         if box != "":
             box = box.split(" ")
             c, x_botleft, y_botleft, x_topright, y_topright = box[0], int(box[1]), int(box[2]), int(box[3]), int(box[4])
             cv2.rectangle(img, (x_botleft, img_height-y_botleft), (x_topright, img_height-y_topright), color=(255, 255, 255), thickness=1)
             cv2.putText(img, strip_accents(c), ((x_botleft + x_topright) // 2 - 3, img_height - y_topright + 30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
-
-    # boxes = pytesseract.image_to_data(img, lang='hun')
+    #
+    # boxes = pytesseract.image_to_data(img, lang='hun', config='--psm 7')
     # for box in boxes.splitlines():
     #     print(box)
 
@@ -50,7 +75,7 @@ def put_bboxes(cv_image):
 
 
 def get_title(cv_image):
-    return pytesseract.image_to_string(cv_image, lang='hun')
+    return pytesseract.image_to_string(cv_image, lang='hun', config='--psm 7')  # psm 7: treat image as a single line of text
 
 
 def linkify(filepath: str):
@@ -71,6 +96,7 @@ def linkify(filepath: str):
             - save a (title, page_number) for later for the contents page
     """
 
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     doc = fitz.open(filepath)  # open the pdf
     page_num = 1
     for page in doc:
@@ -79,9 +105,9 @@ def linkify(filepath: str):
         img = preprocess_image(img)  # prepare image for OCR
 
         print(get_title(img))
-        img = put_bboxes(img)
+        # img = put_bboxes(img)
 
-        cv2.imshow("Image", img)
+        cv2.imshow("processed", img)
         cv2.waitKey(0)
 
         page_num += 1
