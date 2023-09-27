3f2ab4f7e9af292ab2ef0994067b8fde8b6870f3
csongorszasz <szasz.csongor1@gmail.com>
2023-09-27 14:09:39 +0300

    [Correction] Made it possible for the table of contents to have accented titles sorted correctly

--- linkifier.py
+++ linkifier.py
@@ -18,18 +18,30 @@ def init_tesseract():
 
 
 def add_contents_page_to_doc(doc, titles: list[tuple[int, str]]):
-    toc_list = []
+    accented = []
+    toc_stripped = []
+    idx = 0
     for title in titles:
-        toc_list.append([1, strip_accents(title[1]), title[0]+1])
-    toc_list.sort(key=lambda x: x[1])  # sort lists by title
-    doc.set_toc(toc=toc_list, collapse=0)
-    print(f"Table of contents has been added")
+        title_str = title[1]
+        page = title[0]+1
+        accented.append(title_str)
+        toc_stripped.append([strip_accents(title_str), page, idx])
+        idx += 1
+    toc_stripped.sort(key=lambda x: x[0])
+
+    toc_final = []
+    for i in range(len(titles)):
+        title = accented[toc_stripped[i][2]]
+        page = toc_stripped[i][1]
+        toc_final.append([1, title, page])
+    doc.set_toc(toc=toc_final, collapse=0)
+    print(f"Table of contents added")
 
 
 def add_title_to_doc(doc, page_num: int, title: str):
     page = doc[page_num]
     page.insert_text(fitz.Point(30, 30), strip_accents(title), overlay=False)
-    print(f"Title '{title}' added to page {page_num+1}")
+    print(f"Title added to page {page_num+1}: '{title}'")
 
 
 def add_titles_to_doc(doc, titles: list[tuple[int, str]]):
@@ -38,7 +50,7 @@ def add_titles_to_doc(doc, titles: list[tuple[int, str]]):
 
 
 def gather_titles(doc) -> list[tuple[int, str]]:
-    doc.select([1,2,33,34,35,36,37,38])
+    # doc.select([1,2,33,34,35,36,37,38])
 
     titles = []  # a list of (page_num, title) pairs
     for page in doc:
@@ -49,7 +61,7 @@ def gather_titles(doc) -> list[tuple[int, str]]:
         for title in titles_results:
             title = title[0].upper() + title[1:]
             titles.append((page.number, title))
-            print(title)
+            print(f"OCR: {title}")
 
         # cv2.imshow("img", img)
         # cv2.waitKey(0)
@@ -85,9 +97,13 @@ def linkify(filepath: str):
     doc = fitz.open(filepath, filetype='.pdf')  # open the pdf
     print(f"> Opened file: {filepath}")
 
-    titles = gather_titles(doc)
-    add_titles_to_doc(doc, titles)
-    add_contents_page_to_doc(doc, titles)
+    try:
+        titles = gather_titles(doc)
+        add_titles_to_doc(doc, titles)
+        add_contents_page_to_doc(doc, titles)
+    except:
+        print("> Error: Something went wrong")
+        pass
 
     doc.save(f"{filepath[:-4]}-feldolgozott.pdf")  # save the modified pdf with a suffix
     doc.close()  # close the pdf
